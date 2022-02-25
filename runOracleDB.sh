c709e64bd8efff5ca91f9890ea11baba2aad9845
Ignacio <santitax89@gmail.com>
2022-02-25 16:40:39 -0300

    Extended RunOracleDB Script to be more bervose and check if the DB is already running before start it

--- runOracleDB.sh
+++ runOracleDB.sh
@@ -4,4 +4,13 @@ SCRIPT_PATH=$(pwd)
 
 [ "$(whoami)" != "root" ] && exec sudo sh $SCRIPT_PATH/$0
 
-sudo docker start -a oracle-12c-r2 > /dev/null &
+dockerCheck=$(docker container ls -a | grep oracle-db-12c-r2 | grep Up)
+
+if [ -z "$dockerCheck" ]; then
+    echo "Docker - oracle-db-12c-r2 - Is NOT Running."
+    echo "Launching Docker Container - oracle-db-12c-r2"
+    sudo docker start -a oracle-12c-r2 > /dev/null &
+else
+    echo "Docker - oracle-db-12c-r2 - Is Running."
+fi
+
